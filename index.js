2b9c4980c6b8c9aac0b65505f2eb2645ffe59f05
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-11-02 12:58:16 +0530

    Fixed parseInt for the API

--- index.js
+++ index.js
@@ -18,16 +18,18 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.get('/api/courses/:id', (req,res) => {
-    const course = courses.find(c => c.id === req.params.id);
-    if(!course) res.status(404).send('Course with given id is not found!!!');
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('Course with given id is not found!!!');
     res.send(course);
 });
 
-app.post('/api/courses/:id', (req, res) => {
-    const { error } = validateSchema(req.body.name);
+app.post('/api/courses/', (req, res) => {
+    const { error } = validateSchema(req.body);
 
-    if(error){res.status(400).send('Name should be of at least 3 characters')}
-
+    if(error) {
+        console.log(error);
+        return res.status(400).send('Name can have 3 to 20 characters');
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -38,19 +40,19 @@ app.post('/api/courses/:id', (req, res) => {
 });
 
 app.put('/api/courses/:id', (req, res) => {
-   const course = courses.find(c => c.id === req.params.id);
-   if(!course) {res.status(404).send('Course with given id is not found!!!')}
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if(!course) return res.status(404).send('Course with given id is not found!!!');
 
-   const { error } = validateSchema(req.body.name);
-   if(error) {res.status(400).send('Name should be of at least 3 characters')}
+   const { error } = validateSchema(req.body);
+   if(error) return res.status(400).send('Name can 3 to 20 characters')
 
-   course.name = name;
+   course.name = req.body.name;
    res.send(course);
 });
 
-app.delete('api/courses/:id', (req, res) => {
-   const course = courses.find(c => c.id === req.params.id);
-   if(!course) {res.status(404).send('Course with given id is not found')}
+app.delete('/api/courses/:id', (req, res) => {
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if(!course) return res.status(404).send('Course with given id is not found');
 
    const index = courses.indexOf(course);
    courses.splice(index, 1);
@@ -59,7 +61,7 @@ app.delete('api/courses/:id', (req, res) => {
 
 function validateSchema(course){
     const schema = Joi.object({
-        name: Joi.string().min(3).required(),
+        name: Joi.string().min(3).max(20).required(),
     })
 
     return schema.validate(course);
